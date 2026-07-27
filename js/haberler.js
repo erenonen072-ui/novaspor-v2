e099b140162d3fb15a45ed46086867d150de46cc
erenonen072-ui <erenonen072@gmail.com>
2026-07-27 21:58:39 +0300

    Add news articles to haberler.js

---
+++ js/haberler.js
@@ -0,0 +1,45 @@
+// ======================
+// NOVASPOR HABERLER
+// ======================
+
+const haberler = [
+
+{
+baslik:"Son Dakika: Galatasaray Venezia ile hazırlık maçına çıkıyor",
+
+aciklama:"Galatasaray'ın hazırlık maçı öncesi tüm gelişmeler NovaSpor'da.",
+
+resim:"images/haber1.jpg.png",
+
+link:"https://sites.google.com/view/novassporr/haberler_1/son-dakika-galatasaray-venezia-ile-hazirlik-macina-cikiyor",
+
+kategori:"TRANSFER"
+},
+
+
+{
+baslik:"Fenerbahçe'de transfer çalışmaları",
+
+aciklama:"Sarı lacivertli ekipte yeni gelişmeler yaşanıyor.",
+
+resim:"images/haber3.jpg",
+
+link:"#",
+
+kategori:"TRANSFER"
+},
+
+
+{
+baslik:"Süper Lig'den son haberler",
+
+aciklama:"Ligde yaşanan önemli gelişmeler.",
+
+resim:"images/haber4.jpg",
+
+link:"#",
+
+kategori:"SÜPER LİG"
+}
+
+];
