b27536f3a5b81521a988fe65757d0b9973b80497
erenonen072-ui <erenonen072@gmail.com>
2026-07-27 23:30:17 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -59,18 +59,25 @@ Devamını Oku →
 // ======================
 
 
+// ======================
+// BÜYÜK MANŞET SLIDER
+// ======================
+
+
+let manseIndex = 0;
+
+
 const buyukBaslik = document.querySelector("#buyuk-baslik");
-
 const buyukAciklama = document.querySelector("#buyuk-aciklama");
-
 const buyukResim = document.querySelector(".featured img");
+const buyukLink = document.querySelector("#buyuk-link");
 
 
 
-if(buyukBaslik && typeof haberler !== "undefined"){
+function mansetGoster(){
 
 
-let haber = haberler[0];
+let haber = haberler[manseIndex];
 
 
 buyukBaslik.innerHTML = haber.baslik;
@@ -79,12 +86,63 @@ buyukBaslik.innerHTML = haber.baslik;
 buyukAciklama.innerHTML = haber.aciklama;
 
 
-if(buyukResim){
-
 buyukResim.src = haber.resim;
+
+
+buyukLink.href = haber.link;
+
 
 }
 
+
+
+
+
+document.querySelector("#sonrakiHaber").onclick = function(){
+
+
+manseIndex++;
+
+
+if(manseIndex >= haberler.length){
+
+manseIndex = 0;
+
+}
+
+
+mansetGoster();
+
+
+};
+
+
+
+
+
+document.querySelector("#oncekiHaber").onclick = function(){
+
+
+manseIndex--;
+
+
+if(manseIndex < 0){
+
+manseIndex = haberler.length - 1;
+
+}
+
+
+mansetGoster();
+
+
+};
+
+
+
+
+
+mansetGoster();
 }
 // ======================
 // POPÜLER HABERLER
