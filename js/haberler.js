a8b500f375beb005dab1df451743eea4dde66c53
erenonen072-ui <erenonen072@gmail.com>
2026-07-27 22:37:33 +0300

    Update haberler.js

--- js/haberler.js
+++ js/haberler.js
@@ -11,7 +11,7 @@ baslik:"Son Dakika: Galatasaray Venezia ile hazırlık maçına çıkıyor",
 
 aciklama:"Galatasaray yeni sezon hazırlıkları kapsamında Venezia ile oynayacağı hazırlık maçı öncesi son gelişmeler NovaSpor'da.",
 
-resim:"99b16b21-3dd7-43f4-a1b5-880d80927cd0.png",
+resim:"images/99b16b21-3dd7-43f4-a1b5-880d80927cd0.png",
 
 link:"https://sites.google.com/view/novassporr/haberler_1/son-dakika-galatasaray-venezia-ile-haz%C4%B1rl%C4%B1k-ma%C3%A7%C4%B1na-%C3%A7%C4%B1k%C4%B1yor?authuser=0",
 
