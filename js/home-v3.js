779c06498a0e686df814f3b46c50a524503cbd92
erenonen072-ui <erenonen072@gmail.com>
2026-07-29 03:49:43 +0300

    Update home-v3.js

--- js/home-v3.js
+++ js/home-v3.js
@@ -1,1 +1,67 @@
+document.addEventListener("DOMContentLoaded",()=>{
 
+    haberleriGoster(
+        "superlig-container",
+        superligHaberleri
+    );
+
+
+    haberleriGoster(
+        "avrupa-container",
+        avrupaHaberleri
+    );
+
+
+    haberleriGoster(
+        "basketbol-container",
+        basketbolHaberleri
+    );
+
+
+    haberleriGoster(
+        "transfer-container",
+        transferHaberleri
+    );
+
+});
+
+
+
+function haberleriGoster(alan, liste){
+
+const container=document.getElementById(alan);
+
+if(!container) return;
+
+
+container.innerHTML="";
+
+
+liste.forEach(haber=>{
+
+
+container.innerHTML += `
+
+<div class="haber-card">
+
+<img src="${haber.resim}" loading="lazy">
+
+<div>
+
+<span>${haber.kategori}</span>
+
+<h3>${haber.baslik}</h3>
+
+<p>${haber.aciklama}</p>
+
+</div>
+
+</div>
+
+`;
+
+
+});
+
+
+}
