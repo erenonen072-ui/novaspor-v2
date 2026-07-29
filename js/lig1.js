44d6f7e26c12414cfae77ade6165fb1ea74705e3
erenonen072-ui <erenonen072@gmail.com>
2026-07-29 18:05:17 +0300

    Update lig1.js

--- js/lig1.js
+++ js/lig1.js
@@ -7,26 +7,40 @@ window.lig1Haberleri = [
 {
     kategori: "TRANSFER",
     baslik: "Batman Petrolspor'dan Güçlü Takviye",
-    aciklama: "Kulüp yeni sezon öncesi kadrosunu güçlendirmeye devam ediyor.",
+    aciklama: "Batman Petrolspor yeni sezon öncesi kadrosunu güçlendirmek için transfer çalışmalarına devam ediyor.",
     resim: "images/batman.png",
     link: "#"
 },
 
-
 {
     kategori: "TRANSFER",
     baslik: "Vanspor FK'den Dev Transfer Hamlesi",
-    aciklama: "Vanspor FK yeni sezon planlamaları kapsamında önemli bir adım attı.",
+    aciklama: "Vanspor FK yeni sezon hedefleri doğrultusunda önemli bir transfer hamlesine hazırlanıyor.",
     resim: "images/vanspor.png",
     link: "#"
 },
 
-
 {
     kategori: "GÜNDEM",
     baslik: "Sarıyer'den Çifte Takviye",
-    aciklama: "Sarıyer yeni sezon öncesi çalışmalarını sürdürüyor.",
+    aciklama: "Sarıyer yönetimi yeni sezon kadro planlaması kapsamında iki önemli oyuncuyla anlaşmaya vardı.",
     resim: "images/sariyer.png",
+    link: "#"
+},
+
+{
+    kategori: "MAÇ",
+    baslik: "1. Lig'de Yeni Sezon Heyecanı Başlıyor",
+    aciklama: "Takımlar sezon öncesi hazırlıklarını sürdürürken taraftarlar yeni sezonu bekliyor.",
+    resim: "images/lig1.png",
+    link: "#"
+},
+
+{
+    kategori: "GÜNDEM",
+    baslik: "1. Lig Takımlarında Sezon Hazırlıkları",
+    aciklama: "Kulüpler yeni sezon öncesi kamp çalışmalarına hız verdi.",
+    resim: "images/lig1.png",
     link: "#"
 }
 
