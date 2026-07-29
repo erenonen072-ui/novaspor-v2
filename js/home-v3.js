bb96d279c9a50fbb9e58f74fe1857c52c8c2d7cb
erenonen072-ui <erenonen072@gmail.com>
2026-07-30 01:02:13 +0300

    Update home-v3.js

--- js/home-v3.js
+++ js/home-v3.js
@@ -242,3 +242,24 @@ if (cookieBox) {
     }
 
 }
+// ==========================
+// DARK MODE
+// ==========================
+
+const darkBtn = document.getElementById("darkBtn");
+
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark");
+}
+
+if(darkBtn){
+    darkBtn.onclick = () => {
+        document.body.classList.toggle("dark");
+
+        if(document.body.classList.contains("dark")){
+            localStorage.setItem("theme","dark");
+        }else{
+            localStorage.setItem("theme","light");
+        }
+    };
+}
