55e0b96db6ae63cb241a71b517dee61c2f3751b0
erenonen072-ui <erenonen072@gmail.com>
2026-07-29 04:11:40 +0300

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,17 +1,29 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
 const darkBtn = document.querySelector(".icon-btn:nth-child(2)");
+
+if(!darkBtn) return;
+
 
 darkBtn.addEventListener("click",()=>{
 
 document.body.classList.toggle("dark");
 
+
 localStorage.setItem(
-"tema",
+"darkMode",
 document.body.classList.contains("dark")
 );
+
 
 });
 
 
-if(localStorage.getItem("tema")=="true"){
+if(localStorage.getItem("darkMode") === "true"){
+
 document.body.classList.add("dark");
+
 }
+
+
+});
