5b15463ae9c272fd3b7006b4cb9f6a3d4c1f5390
erenonen072-ui <erenonen072@gmail.com>
2026-07-29 18:06:33 +0300

    Update lig3.js

--- js/lig3.js
+++ js/lig3.js
@@ -1,1 +1,31 @@
+ // ==========================
+// NOVASPOR - 3. LİG
+// ==========================
+
 window.lig3Haberleri = [
+
+{
+    kategori: "TRANSFER",
+    baslik: "3. Lig'de Transfer Çalışmaları Hız Kazandı",
+    aciklama: "Kulüpler yeni sezon öncesi kadrolarını güçlendirmek için transfer çalışmalarına devam ediyor.",
+    resim: "images/lig3.png",
+    link: "#"
+},
+
+{
+    kategori: "GÜNDEM",
+    baslik: "3. Lig Takımları Yeni Sezona Hazırlanıyor",
+    aciklama: "Takımlar kamp çalışmalarını sürdürürken taraftarlar yeni sezon heyecanını bekliyor.",
+    resim: "images/lig3.png",
+    link: "#"
+},
+
+{
+    kategori: "MAÇ",
+    baslik: "3. Lig'de Büyük Rekabet Başlıyor",
+    aciklama: "Yeni sezonla birlikte mücadele seviyesi yüksek karşılaşmalar futbolseverleri bekliyor.",
+    resim: "images/lig3.png",
+    link: "#"
+}
+
+];
