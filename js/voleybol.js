c980681fabac2e2b7af2996a50de71c691783a93
erenonen072-ui <erenonen072@gmail.com>
2026-07-30 01:36:56 +0300

    Update voleybol.js

--- js/voleybol.js
+++ js/voleybol.js
@@ -1,1 +1,31 @@
+// ==========================
+// NOVASPOR - VOLEYBOL HABERLERİ
+// ==========================
 
+window.voleybolHaberleri = [
+
+{
+    kategori: "TRANSFER",
+    baslik: "Beşiktaş'tan Orta Oyuncu Takviyesi: Aslı Kalaç Siyah-Beyazlı Formayı Giydi",
+    aciklama: "Beşiktaş, yeni sezon kadro planlaması kapsamında milli orta oyuncu Aslı Kalaç'ı kadrosuna kattığını resmen duyurdu. Siyah-beyazlı ekip, tecrübeli voleybolcuyla önemli bir güç kazandı.",
+    resim: "images/VOLEYBOL BJK.jpg",
+    link: "https://sites.google.com/view/novassporr/beşiktaştan-orta-oyuncu-takviyesi-aslı-kalaç-siyah-beyazlı-formayı-giy"
+},
+
+{
+    kategori: "TRANSFER",
+    baslik: "Fenerbahçe Medicana'dan Dünya Yıldızı Hamlesi: Brenda Castillo Resmen Sarı-Lacivertli",
+    aciklama: "Fenerbahçe Medicana, dünyanın en başarılı liberolarından Brenda Castillo'yu kadrosuna kattığını açıkladı. Transfer voleybol dünyasında büyük ses getirdi.",
+    resim: "images/VOLEYBOL FB.jpg",
+    link: "https://sites.google.com/view/novassporr/fenerbahçe-medicanadan-dünya-yıldızı-hamlesi-brenda-castillo-resmen-sa?authuser=0"
+},
+
+{
+    kategori: "TRANSFER",
+    baslik: "Galatasaray Daikin'den Ayça Aykaç Hamlesi: Milli Libero Resmen Sarı-Kırmızılı",
+    aciklama: "Galatasaray Daikin, A Milli Kadın Voleybol Takımı'nın başarılı liberosu Ayça Aykaç'ı renklerine bağladı. Sarı-kırmızılılar yeni sezona iddialı bir kadroyla hazırlanıyor.",
+    resim: "images/VOLEYBOL GS.jpg",
+    link: "https://sites.google.com/view/novassporr/galatasaray-daikinden-ayça-aykaç-hamlesi-milli-libero-resmen-sarı-kırm?authuser=0"
+}
+
+];
