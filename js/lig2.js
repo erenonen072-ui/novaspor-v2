e7ded8524dd06d626af65f30ae2f8986cd0027d5
erenonen072-ui <erenonen072@gmail.com>
2026-07-29 18:05:45 +0300

    Update transfer news and add new season excitement

--- js/lig2.js
+++ js/lig2.js
@@ -7,64 +7,25 @@ window.lig2Haberleri = [
 {
     kategori: "TRANSFER",
     baslik: "Menemen FK'dan Orta Sahaya Güçlü Takviye: Deneyimli İsim Resmen Kadroda",
+    aciklama: "Menemen FK yeni sezon öncesi orta saha hattını güçlendirmek için önemli bir transfer gerçekleştirdi.",
     resim: "images/MENEMEN.png",
-    link: "https://sites.google.com/view/novassporr/2-lig-haberleri/menemen-fkdan-orta-sahaya-güçlü-takviye-deneyimli-isim-resmen-kadroda",
-    saat: "Az önce"
+    link: "https://sites.google.com/view/novassporr/2-lig-haberleri/menemen-fkdan-orta-sahaya-güçlü-takviye-deneyimli-isim-resmen-kadroda"
 },
 
 {
     kategori: "TRANSFER",
     baslik: "Çorum FK'dan Savunmaya Güçlü Takviye: Andrei Borza Resmen İmzayı Attı",
+    aciklama: "Çorum FK savunma hattını güçlendirmek için yaptığı transferle kadrosuna önemli bir isim kattı.",
     resim: "images/ÇORUM.png",
-    link: "https://sites.google.com/view/novassporr/2-lig-haberleri/çorum-fkdan-savunmaya-güçlü-takviye-andrei-borza-resmen-imzayı-attı?authuser=0",
-    saat: "Az önce"
+    link: "https://sites.google.com/view/novassporr/2-lig-haberleri/çorum-fkdan-savunmaya-güçlü-takviye-andrei-borza-resmen-imzayı-attı?authuser=0"
 },
+
 {
     kategori: "2. LİG",
     baslik: "2. Lig'de Yeni Sezon Heyecanı Başlıyor",
+    aciklama: "Takımlar yeni sezon hazırlıklarını sürdürürken taraftarlar büyük heyecanla başlangıcı bekliyor.",
     resim: "images/nesine-2-lig-page.jpg",
-    link: "https://sites.google.com/view/novassporr/2-lig-haberleri/2-ligde-yeni-sezon-heyecanı-başlıyor?authuser=0",
-    saat: "Bugün"
-},
+    link: "https://sites.google.com/view/novassporr/2-lig-haberleri/2-ligde-yeni-sezon-heyecanı-başlıyor?authuser=0"
+}
 
 ];
-
-const lig2Container = document.getElementById("lig2-container");
-
-if (lig2Container) {
-
-    lig2Haberleri.forEach(haber => {
-
-        lig2Container.innerHTML += `
-
-        <article class="news-card">
-
-            <img src="${haber.resim}" alt="${haber.baslik}">
-
-            <div class="news-info">
-
-                <span class="news-tag">${haber.kategori}</span>
-
-                <h3>${haber.baslik}</h3>
-
-                <div class="news-bottom">
-
-                    <span>🕒 ${haber.saat}</span>
-
-                    <a href="${haber.link}" target="_blank">
-
-                        Haberi Oku
-
-                    </a>
-
-                </div>
-
-            </div>
-
-        </article>
-
-        `;
-
-    });
-
-}
