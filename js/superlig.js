aed28f43b313ef2508e21b7dc4d504e92d8475b4
erenonen072-ui <erenonen072@gmail.com>
2026-07-29 04:07:58 +0300

    Update superlig.js

--- js/superlig.js
+++ js/superlig.js
@@ -1,1 +1,24 @@
+const superligHaberleri = [
 
+{
+kategori:"Süper Lig",
+baslik:"Galatasaray'dan Dev Transfer Hamlesi",
+aciklama:"Sarı-kırmızılı ekip yeni sezon öncesi çalışmalarını sürdürüyor.",
+resim:"images/haber1.jpg.png"
+},
+
+{
+kategori:"Süper Lig",
+baslik:"Fenerbahçe Kampında Son Durum",
+aciklama:"Sarı-lacivertliler hazırlıklarına devam ediyor.",
+resim:"images/haber2.jpg.png"
+},
+
+{
+kategori:"Süper Lig",
+baslik:"Beşiktaş'tan Yeni Sezon Hazırlığı",
+aciklama:"Siyah-beyazlı ekip kadro çalışmalarını sürdürüyor.",
+resim:"images/haber3.jpg.png"
+}
+
+];
