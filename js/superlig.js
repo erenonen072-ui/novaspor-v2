313555fb8411b326993d5a106f380a2bada93cec
erenonen072-ui <erenonen072@gmail.com>
2026-07-29 17:44:52 +0300

    Update superlig.js

--- js/superlig.js
+++ js/superlig.js
@@ -1,24 +1,33 @@
+// ==========================
+// NOVASPOR - SÜPER LİG
+// ==========================
+
 window.superLigHaberleri = [
 
 {
-kategori:"Süper Lig",
-baslik:"Galatasaray'dan Dev Transfer Hamlesi",
-aciklama:"Sarı-kırmızılı ekip yeni sezon öncesi çalışmalarını sürdürüyor.",
-resim:"images/haber1.jpg.png"
+    kategori: "TRANSFER",
+    baslik: "Fenerbahçe'den Mason Greenwood Hamlesi Resmileşti",
+    aciklama: "Fenerbahçe transfer çalışmalarında önemli bir gelişmeye imza attı.",
+    resim: "images/hq720.jpg",
+    link: "https://sites.google.com/view/novassporr/haberler_1/fenerbahçeden-mason-greenwood-hamlesi-resmileşti?authuser=0"
 },
 
-{
-kategori:"Süper Lig",
-baslik:"Fenerbahçe Kampında Son Durum",
-aciklama:"Sarı-lacivertliler hazırlıklarına devam ediyor.",
-resim:"images/haber2.jpg.png"
-},
 
 {
-kategori:"Süper Lig",
-baslik:"Beşiktaş'tan Yeni Sezon Hazırlığı",
-aciklama:"Siyah-beyazlı ekip kadro çalışmalarını sürdürüyor.",
-resim:"images/haber3.jpg.png"
+    kategori: "TRANSFER",
+    baslik: "Beşiktaş'tan Dev Transfer: Leandro Trossard Resmen Siyah-Beyazlı",
+    aciklama: "Beşiktaş kadrosunu güçlendirmek için önemli bir hamle yaptı.",
+    resim: "images/leandro-trossard-saglik-kontrolunden-gecti.jpg",
+    link: "https://sites.google.com/view/novassporr/haberler_1/beşiktaştan-dev-transfer-leandro-trossard-resmen-siyah-beyazlı?authuser=0"
+},
+
+
+{
+    kategori: "SON DAKİKA",
+    baslik: "Galatasaray'dan Can Uzun Hamlesi",
+    aciklama: "Galatasaray transfer çalışmalarına hız verdi.",
+    resim: "images/can uzun.jpg",
+    link: "https://sites.google.com/view/novassporr/haberler_1/son-dakika-galatasaraydan-can-uzun-hamlesi?authuser=0"
 }
 
 ];
