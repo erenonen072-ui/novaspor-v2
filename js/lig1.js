d2c4fbb0d71b288aa0636cf5adbb0e53dfc6ce31
erenonen072-ui <erenonen072@gmail.com>
2026-07-29 17:55:42 +0300

    Update lig1.js

--- js/lig1.js
+++ js/lig1.js
@@ -1,1 +1,33 @@
+// ==========================
+// NOVASPOR - 1. LİG
+// ==========================
+
 window.lig1Haberleri = [
+
+{
+    kategori: "TRANSFER",
+    baslik: "Batman Petrolspor'dan Güçlü Takviye",
+    aciklama: "Kulüp yeni sezon öncesi kadrosunu güçlendirmeye devam ediyor.",
+    resim: "images/batman.png",
+    link: "#"
+},
+
+
+{
+    kategori: "TRANSFER",
+    baslik: "Vanspor FK'den Dev Transfer Hamlesi",
+    aciklama: "Vanspor FK yeni sezon planlamaları kapsamında önemli bir adım attı.",
+    resim: "images/vanspor.png",
+    link: "#"
+},
+
+
+{
+    kategori: "GÜNDEM",
+    baslik: "Sarıyer'den Çifte Takviye",
+    aciklama: "Sarıyer yeni sezon öncesi çalışmalarını sürdürüyor.",
+    resim: "images/sariyer.png",
+    link: "#"
+}
+
+];
