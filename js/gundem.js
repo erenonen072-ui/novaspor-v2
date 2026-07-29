39eac95cb5d0a3213318e033f8056f499e293b43
erenonen072-ui <erenonen072@gmail.com>
2026-07-29 16:46:12 +0300

    Update gundem.js

--- js/gundem.js
+++ js/gundem.js
@@ -1,1 +1,40 @@
+// ======================================
+// NOVASPOR GÜNDEM
+// ======================================
 
+const gundemHaberleri = [
+    superLigHaberleri[0],
+    transferHaberleri[0],
+    lig2Haberleri[0],
+    avrupaHaberleri[0]
+];
+
+const gundemContainer = document.getElementById("gundem-container");
+
+if (gundemContainer) {
+
+    gundemContainer.innerHTML = "";
+
+    gundemHaberleri.forEach(haber => {
+
+        gundemContainer.innerHTML += `
+
+            <a href="${haber.link}">
+
+                <img src="${haber.resim}" alt="${haber.baslik}">
+
+                <div>
+
+                    <span>${haber.kategori}</span>
+
+                    <h4>${haber.baslik}</h4>
+
+                </div>
+
+            </a>
+
+        `;
+
+    });
+
+}
