cca59274ab8ec4ca486ac76577283e2e68b62b3c
erenonen072-ui <erenonen072@gmail.com>
2026-07-30 03:26:15 +0300

    Update basketbol.js

--- js/basketbol.js
+++ js/basketbol.js
@@ -5,6 +5,7 @@ kategori:"Basketbol",
 baslik:"Cedi Osman Yeni Sezon Öncesi Form Tutuyor!",
 aciklama:"Hedef EuroLeague'de zirve.",
 resim:"images/🏀 Cedi Osman Yeni Sezon Öncesi Form Tutuyor! Hedef EuroLeague'de Zirve.png"
+link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/cedi-osman-yeni-sezon-%C3%B6ncesi-form-tutuyor-hedef-euroleaguede-zirve?authuser=0"
 },
 
 {
@@ -12,6 +13,7 @@ kategori:"Basketbol",
 baslik:"Fenerbahçe Beko'dan Güç Gösterisi!",
 aciklama:"Yeni transferlerle hedef Avrupa'nın zirvesi.",
 resim:"images/🏀 Fenerbahçe Beko'dan Güç Gösterisi! Yeni Transferlerle Hedef Bir Kez Daha Avrupa'nın Zirvesi.png"
+link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/fenerbah%C3%A7e-bekodan-g%C3%BC%C3%A7-g%C3%B6sterisi-yeni-transferlerle-hedef-bir-kez-daha?authuser=0"
 },
 
 {
@@ -19,6 +21,7 @@ kategori:"Basketbol",
 baslik:"Shane Larkin'den İddialı Açıklamalar!",
 aciklama:"Kupalar için mücadele edeceğiz.",
 resim:"images/🏀 Shane Larkin'den Yeni Sezon Öncesi İddialı Açıklamalar! Kupalar İçin Mücadele Edeceğiz.png"
+link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/shane-larkinden-yeni-sezon-%C3%B6ncesi-iddial%C4%B1-a%C3%A7%C4%B1klamalar-kupalar-i%C3%A7in-m%C3%BC?authuser=0"
 }
 
 ];
