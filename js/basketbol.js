9a3bd171fe532cc278b43d76c784c38faa96f235
erenonen072-ui <erenonen072@gmail.com>
2026-07-30 03:51:23 +0300

    Update basketbol.js

--- js/basketbol.js
+++ js/basketbol.js
@@ -12,7 +12,7 @@ link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/fenerbah%C3
 kategori:"Basketbol",
 baslik:"Alperen Şengün'den Sezon Öncesi İddialı Mesaj!",
 aciklama:"Daha güçlü döneceğim.",
-resim:"images/Alperen Şengün'ün sezon öncesi iddialı mesajı.png
+resim:"images/Alperen Şengün'ün sezon öncesi iddialı mesajı.png",
 link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/alperen-%C5%9Feng%C3%BCnden-sezon-%C3%B6ncesi-iddial%C4%B1-mesaj-daha-g%C3%BC%C3%A7l%C3%BC-d%C3%B6nece%C4%9Fim?authuser=0"
 },
 
@@ -20,7 +20,7 @@ link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/alperen-%C5
 kategori:"Basketbol",
 baslik:"Cedi Osman Yeni Sezon Öncesi Form Tutuyor!",
 aciklama:"Hedef EuroLeague'de zirve.",
-resim:"images/"🏀 Cedi Osman Yeni Sezon Öncesi Form Tutuyor Hedef EuroLeague'de Zirve.png",
+resim:"images/🏀 Cedi Osman Yeni Sezon Öncesi Form Tutuyor! Hedef EuroLeague'de Zirve.png",
 link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/cedi-osman-yeni-sezon-%C3%B6ncesi-form-tutuyor-hedef-euroleaguede-zirve?authuser=0"
 },
 
