31b99a07458308ebed2a63efe8ee9e175004a1a1
erenonen072-ui <erenonen072@gmail.com>
2026-07-30 15:02:17 +0300

    Update superlig.js

--- js/superlig.js
+++ js/superlig.js
@@ -28,6 +28,13 @@ window.superLigHaberleri = [
     aciklama: "Galatasaray transfer çalışmalarına hız verdi.",
     resim: "images/can uzun.jpg",
     link: "https://sites.google.com/view/novassporr/haberler_1/son-dakika-galatasaraydan-can-uzun-hamlesi?authuser=0"
-}
+},
+{
+    kategori: "TRANSFER",
+    baslik: "Fenerbahçe'den Serhou Guirassy Hamlesi! Golcü Transferinde Geri Sayım",
+    aciklama: "Fenerbahçe, yeni sezon öncesinde gol yollarını güçlendirmek için Serhou Guirassy transferinde önemli mesafe kat etti. Görüşmelerin olumlu ilerlediği öğrenildi.",
+    resim: "images/ChatGPT Image 30 Tem 2026 14_26_53.png",
+    link: "https://sites.google.com/view/novassporr/fenerbah%C3%A7eden-serhou-guirassy-hamlesi-golc%C3%BC-transferinde-geri-say%C4%B1m?authuser=0"
+},
 
 ];
