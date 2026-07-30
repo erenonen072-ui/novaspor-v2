bdc4dbf5c2354a9cd9f3dd39ebf2d47baae25cc5
erenonen072-ui <erenonen072@gmail.com>
2026-07-30 14:56:34 +0300

    Add transfer news entry for Elazığspor

--- js/lig2.js
+++ js/lig2.js
@@ -26,13 +26,13 @@ window.lig2Haberleri = [
     aciklama: "Takımlar yeni sezon hazırlıklarını sürdürürken taraftarlar büyük heyecanla başlangıcı bekliyor.",
     resim: "images/nesine-2-lig-page.jpg",
     link: "https://sites.google.com/view/novassporr/2-lig-haberleri/2-ligde-yeni-sezon-heyecanı-başlıyor?authuser=0"
-}
+},
 {
     kategori: "TRANSFER",
     baslik: "Elazığspor'dan Savunmaya Güçlü Takviye",
     aciklama: "Elazığspor, yeni sezon öncesinde savunma hattını genç ve gelecek vadeden bir isimle güçlendirdi. Transfer, camiada büyük heyecan yarattı.",
     resim: "images/ChatGPT Image 30 Tem 2026 14_35_54.png",
     link: "https://sites.google.com/view/novassporr/elaz%C4%B1%C4%9Fspordan-savunmaya-g%C3%BC%C3%A7l%C3%BC-takviye-galatasaray%C4%B1n-gen%C3%A7-yetene%C4%9Fi-resmen?authuser=0"
-}
+},
 
 ];
