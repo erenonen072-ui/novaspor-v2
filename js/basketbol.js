a8b77e08f43a6e7ab9de78c629a186e417a5adea
erenonen072-ui <erenonen072@gmail.com>
2026-07-30 03:48:40 +0300

    Update basketbol.js

--- js/basketbol.js
+++ js/basketbol.js
@@ -4,7 +4,7 @@ window.basketbolHaberleri = [
 kategori:"Basketbol",
 baslik:"Fenerbahçe Beko'dan Güç Gösterisi!",
 aciklama:"Yeni transferlerle hedef Avrupa'nın zirvesi.",
-resim:"images/fenerbahce-beko.png",
+resim:"images/🏀Fenerbahçe Beko'dan Güç Gösterisi! Yeni Transferlerle Hedef Bir Kez Daha Avrupa'nın Zirvesi.png",
 link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/fenerbah%C3%A7e-bekodan-g%C3%BC%C3%A7-g%C3%B6sterisi-yeni-transferlerle-hedef-bir-kez-daha?authuser=0"
 },
 
@@ -12,7 +12,7 @@ link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/fenerbah%C3
 kategori:"Basketbol",
 baslik:"Alperen Şengün'den Sezon Öncesi İddialı Mesaj!",
 aciklama:"Daha güçlü döneceğim.",
-resim:"images/alperen-sengun.png",
+resim:"images/Alperen Şengün'ün sezon öncesi iddialı mesajı.png
 link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/alperen-%C5%9Feng%C3%BCnden-sezon-%C3%B6ncesi-iddial%C4%B1-mesaj-daha-g%C3%BC%C3%A7l%C3%BC-d%C3%B6nece%C4%9Fim?authuser=0"
 },
 
@@ -20,7 +20,7 @@ link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/alperen-%C5
 kategori:"Basketbol",
 baslik:"Cedi Osman Yeni Sezon Öncesi Form Tutuyor!",
 aciklama:"Hedef EuroLeague'de zirve.",
-resim:"images/cedi-osman.png",
+resim:"images/"🏀 Cedi Osman Yeni Sezon Öncesi Form Tutuyor Hedef EuroLeague'de Zirve.png",
 link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/cedi-osman-yeni-sezon-%C3%B6ncesi-form-tutuyor-hedef-euroleaguede-zirve?authuser=0"
 },
 
@@ -28,7 +28,7 @@ link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/cedi-osman-
 kategori:"Basketbol",
 baslik:"Shane Larkin'den Yeni Sezon Öncesi İddialı Açıklamalar!",
 aciklama:"Kupalar için mücadele edeceğiz.",
-resim:"images/shane-larkin.png",
+resim:"images/🏀 Shane Larkin'den Yeni Sezon Öncesi İddialı Açıklamalar! Kupalar İçin Mücadele Edeceğiz.png",
 link:"https://sites.google.com/view/novassporr/basketbol-haberleri_1/shane-larkinden-yeni-sezon-%C3%B6ncesi-iddial%C4%B1-a%C3%A7%C4%B1klamalar-kupalar-i%C3%A7in-m%C3%BC?authuser=0"
 }
 
