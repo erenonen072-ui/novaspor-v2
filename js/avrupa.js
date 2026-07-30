0d5b6bd986c2a2b63e3a807c1673f13836eb1af5
erenonen072-ui <erenonen072@gmail.com>
2026-07-30 03:56:10 +0300

    Update avrupa.js

--- js/avrupa.js
+++ js/avrupa.js
@@ -9,7 +9,7 @@ kategori:"AVRUPA KUPALARI",
 baslik:"Vinícius Júnior İçin Avrupa Devleri Karşı Karşıya! Brezilyalı Yıldızın Geleceği Merak Konusu Oldu",
 aciklama:"Real Madrid'in yıldızı Vinícius Júnior için Avrupa'nın dev kulüplerinin ilgisi devam ediyor. Brezilyalı futbolcunun geleceği transfer döneminin en çok konuşulan konularından biri oldu.",
 resim:"images/AVRUPA FUTBOLCUSU.jpg",
-link:"https://sites.google.com/view/novassporr/vin%C3%ADcius-j%C3%BAnior-i%C3%A7in-avrupa-devleri-kar%C5%9F%C4%B1-kar%C5%9F%C4%B1ya-brezilyal%C4%B1-y%C4%B1ld%C4%B1z%C4%B1n-gele?authuser=0"
+link:"https://sites.google.com/view/novassporr/avrupa-haberleri/vin%C3%ADcius-j%C3%BAnior-i%C3%A7in-avrupa-devleri-kar%C5%9F%C4%B1-kar%C5%9F%C4%B1ya-brezilyal%C4%B1-y%C4%B1ld%C4%B1z%C4%B1n-gele?authuser=0"
 },
 
 {
@@ -17,7 +17,7 @@ kategori:"AVRUPA KUPALARI",
 baslik:"Bayern Münih'te Yeni Dönem Başlıyor! Alman Devi Avrupa'da Yeniden Güç Peşinde",
 aciklama:"Bayern Münih yeni sezon öncesinde kadrosunu güçlendirmek ve Avrupa'nın zirvesine yeniden çıkmak için çalışmalarını sürdürüyor.",
 resim:"images/FCBATERN.jpg",
-link:"https://sites.google.com/view/novassporr/bayern-m%C3%BCnihte-yeni-d%C3%B6nem-ba%C5%9Fl%C4%B1yor-alman-devi-avrupada-yeniden-g%C3%BC%C3%A7-pe%C5%9Fin?authuser=0"
+link:"https://sites.google.com/view/novassporr/avrupa-haberleri/bayern-m%C3%BCnihte-yeni-d%C3%B6nem-ba%C5%9Fl%C4%B1yor-alman-devi-avrupada-yeniden-g%C3%BC%C3%A7-pe%C5%9Fin?authuser=0"
 },
 
 {
@@ -25,7 +25,7 @@ kategori:"AVRUPA KUPALARI",
 baslik:"Manchester City'de Büyük Yenilenme! Guardiola Avrupa'nın Zirvesine Dönmek İstiyor",
 aciklama:"Manchester City yeni sezon öncesinde kadro yapılanmasına hız verirken Pep Guardiola Avrupa'da yeniden şampiyonluk hedefliyor.",
 resim:"images/MANCİTY.jpg",
-link:"https://sites.google.com/view/novassporr/manchester-cityde-b%C3%BCy%C3%BCk-yenilenme-guardiola-avrupan%C4%B1n-zirvesine-d%C3%B6nmek-i?authuser=0"
+link:"https://sites.google.com/view/novassporr/avrupa-haberleri/manchester-cityde-b%C3%BCy%C3%BCk-yenilenme-guardiola-avrupan%C4%B1n-zirvesine-d%C3%B6nmek-i?authuser=0"
 },
 
 {
@@ -33,7 +33,7 @@ kategori:"AVRUPA KUPALARI",
 baslik:"Real Madrid'den Avrupa'yı Sarsacak Sezon Planı! İspanyol Devi Yeniden Zirveyi Hedefliyor",
 aciklama:"Real Madrid yeni sezonda Avrupa'nın zirvesine çıkmak için kadro planlamasını sürdürüyor. İspanyol devi yine Şampiyonlar Ligi'nin en büyük adaylarından biri.",
 resim:"images/REALAMDERİD.jpg",
-link:"https://sites.google.com/view/novassporr/real-madridden-avrupay%C4%B1-sarsacak-sezon-plan%C4%B1-ispanyol-devi-yeniden-zirve?authuser=0"
+link:"https://sites.google.com/view/novassporr/avrupa-haberleri/real-madridden-avrupay%C4%B1-sarsacak-sezon-plan%C4%B1-ispanyol-devi-yeniden-zirve?authuser=0"
 }
 
 ];
