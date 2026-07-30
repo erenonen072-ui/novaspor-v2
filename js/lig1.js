1ae565cb775703f94624ecbbabdcd1e222631895
erenonen072-ui <erenonen072@gmail.com>
2026-07-30 04:00:04 +0300

    Update lig1.js

--- js/lig1.js
+++ js/lig1.js
@@ -9,7 +9,7 @@ window.lig1Haberleri = [
     baslik: "Batman Petrolspor'dan Güçlü Takviye",
     aciklama: "Batman Petrolspor yeni sezon öncesi kadrosunu güçlendirmek için transfer çalışmalarına devam ediyor.",
     resim: "images/batman.png",
-    link: "#"
+    link: "https://sites.google.com/view/novassporr/tff-1-lig-haberleri_1/batman-petrolspordan-transferde-g%C3%BC%C3%A7-g%C3%B6sterisi-yeni-sezon-%C3%B6ncesi-kadro?authuser=0"
 },
 
 {
@@ -17,7 +17,7 @@ window.lig1Haberleri = [
     baslik: "Vanspor FK'den Dev Transfer Hamlesi",
     aciklama: "Vanspor FK yeni sezon hedefleri doğrultusunda önemli bir transfer hamlesine hazırlanıyor.",
     resim: "images/VANSPOR.jpg",
-    link: "#"
+    link: "https://sites.google.com/view/novassporr/tff-1-lig-haberleri_1/vanspor-fkden-h%C3%BCcum-hatt%C4%B1na-dev-takviye-iv%C3%A1n-c%C3%A9dric-resmen-imzay%C4%B1-att%C4%B1?authuser=0"
 },
 
 {
@@ -25,7 +25,7 @@ window.lig1Haberleri = [
     baslik: "Sarıyer'den Çifte Takviye",
     aciklama: "Sarıyer yönetimi yeni sezon kadro planlaması kapsamında iki önemli oyuncuyla anlaşmaya vardı.",
     resim: "images/Sarıyerspor.png",
-    link: "#"
+    link: "https://sites.google.com/view/novassporr/tff-1-lig-haberleri_1/sar%C4%B1yerden-savunmaya-%C3%A7ifte-takviye-imzalar-pe%C5%9F-pe%C5%9Fe-geldi?authuser=0"
 },
 
 
