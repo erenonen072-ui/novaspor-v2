3ba11637227cc9c447b3bfd307685f306f26cb06
erenonen072-ui <erenonen072@gmail.com>
2026-08-02 01:41:53 +0300

    Update voleybol.js

--- js/voleybol.js
+++ js/voleybol.js
@@ -9,7 +9,7 @@ window.voleybolHaberleri = [
     baslik: "Beşiktaş'tan Orta Oyuncu Takviyesi: Aslı Kalaç Siyah-Beyazlı Formayı Giydi",
     aciklama: "Beşiktaş, yeni sezon kadro planlaması kapsamında milli orta oyuncu Aslı Kalaç'ı kadrosuna kattığını resmen duyurdu. Siyah-beyazlı ekip, tecrübeli voleybolcuyla önemli bir güç kazandı.",
     resim: "images/VOLEYBOL BJK.jpg",
-    link: "https://sites.google.com/view/novassporr/beşiktaştan-orta-oyuncu-takviyesi-aslı-kalaç-siyah-beyazlı-formayı-giy"
+    link: "https://sites.google.com/view/novassporr/voleybol-haberleri/be%C5%9Fikta%C5%9Ftan-orta-oyuncu-takviyesi-asl%C4%B1-kala%C3%A7-siyah-beyazl%C4%B1-formay%C4%B1-giy?authuser=0"
 },
 
 {
@@ -17,7 +17,7 @@ window.voleybolHaberleri = [
     baslik: "Fenerbahçe Medicana'dan Dünya Yıldızı Hamlesi: Brenda Castillo Resmen Sarı-Lacivertli",
     aciklama: "Fenerbahçe Medicana, dünyanın en başarılı liberolarından Brenda Castillo'yu kadrosuna kattığını açıkladı. Transfer voleybol dünyasında büyük ses getirdi.",
     resim: "images/VOLEYBOL FB.jpg",
-    link: "https://sites.google.com/view/novassporr/fenerbahçe-medicanadan-dünya-yıldızı-hamlesi-brenda-castillo-resmen-sa?authuser=0"
+    link: "https://sites.google.com/view/novassporr/voleybol-haberleri/fenerbah%C3%A7e-medicanadan-d%C3%BCnya-y%C4%B1ld%C4%B1z%C4%B1-hamlesi-brenda-castillo-resmen-sa?authuser=0"
 },
 
 {
@@ -25,7 +25,7 @@ window.voleybolHaberleri = [
     baslik: "Galatasaray Daikin'den Ayça Aykaç Hamlesi: Milli Libero Resmen Sarı-Kırmızılı",
     aciklama: "Galatasaray Daikin, A Milli Kadın Voleybol Takımı'nın başarılı liberosu Ayça Aykaç'ı renklerine bağladı. Sarı-kırmızılılar yeni sezona iddialı bir kadroyla hazırlanıyor.",
     resim: "images/VOLEYBOL GS.jpg",
-    link: "https://sites.google.com/view/novassporr/galatasaray-daikinden-ayça-aykaç-hamlesi-milli-libero-resmen-sarı-kırm?authuser=0"
+    link: "https://sites.google.com/view/novassporr/voleybol-haberleri/galatasaray-daikinden-ay%C3%A7a-ayka%C3%A7-hamlesi-milli-libero-resmen-sar%C4%B1-k%C4%B1rm?authuser=0"
 }
 
 ];
