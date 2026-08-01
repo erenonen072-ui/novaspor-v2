3e6e3a910a7e8f638b172e429bcbe6485dcefeb5
erenonen072-ui <erenonen072@gmail.com>
2026-08-01 07:38:21 +0300

    Update players.js

--- js/players.js
+++ js/players.js
@@ -1,3 +1,83 @@
+const players = [
+
+{
+name:"Zeki Çelik",
+image:"images/zeki-celik.jpg",
+position:"Defans",
+club:"Roma",
+birth:"17.02.1997",
+caps:50,
+goals:2,
+number:19
+},
+
+{
+name:"Hakan Çalhanoğlu",
+image:"images/hakan-calhanoglu.jpg",
+position:"Orta Saha",
+club:"Inter",
+birth:"08.02.1994",
+caps:108,
+goals:22,
+number:10
+},
+
+{
+name:"İsmail Yüksek",
+image:"images/ismail-yuksek.jpg",
+position:"Orta Saha",
+club:"Fenerbahçe",
+birth:"26.01.1999",
+caps:34,
+goals:1,
+number:16
+},
+
+{
+name:"Kaan Ayhan",
+image:"images/kaan-ayhan.jpg",
+position:"Orta Saha",
+club:"Galatasaray",
+birth:"10.11.1994",
+caps:74,
+goals:6,
+number:22
+},
+
+{
+name:"Orkun Kökçü",
+image:"images/orkun-kokcu.jpg",
+position:"Orta Saha",
+club:"Beşiktaş",
+birth:"29.12.2000",
+caps:53,
+goals:5,
+number:6
+},
+
+{
+name:"Salih Özcan",
+image:"images/salih-ozcan.jpg",
+position:"Orta Saha",
+club:"Borussia Dortmund",
+birth:"11.01.1998",
+caps:32,
+goals:1,
+number:5
+},
+
+{
+name:"Kenan Yıldız",
+image:"images/kenan-yildiz.jpg",
+position:"Forvet",
+club:"Juventus",
+birth:"04.05.2005",
+caps:21,
+goals:3,
+number:10
+}
+
+];
 /* =========================
    OYUNCU KARTLARI OLUŞTURMA
 ========================= */
