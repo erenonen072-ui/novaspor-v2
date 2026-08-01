380dd0a7f0ed441c3e2dbe839ecb1ebb8df2ebeb
erenonen072-ui <erenonen072@gmail.com>
2026-08-02 00:57:44 +0300

    Update millitakim.js

--- js/millitakim.js
+++ js/millitakim.js
@@ -1,259 +1,45 @@
-const players = [
-
-/* =========================
-   KALECİLER
-========================= */
+const milliTakimHaberleri = [
 
 {
-name:"Altay Bayındır",
-image:"images/altay-bayindir.jpg.jpg",
-position:"Kaleci",
-club:"Manchester United",
-birth:"14.04.1998",
-caps:12,
-goals:0,
-number:12
+baslik:"Montella'dan Dünya Kupası Mesajı",
+aciklama:"A Millî Takım Teknik Direktörü Vincenzo Montella, Dünya Kupası hedeflerinden vazgeçmeyeceklerini belirterek Eylül ayındaki karşılaşmalara en iyi şekilde hazırlanacaklarını söyledi.",
+resim:"images/haber1.jpg.png",
+tarih:"2 Ağustos 2026"
 },
 
 {
-name:"Mert Günok",
-image:"images/mert-gunok.jpg",
-position:"Kaleci",
-club:"Fenerbahçe",
-birth:"01.03.1989",
-caps:37,
-goals:0,
-number:1
+baslik:"Hakan Çalhanoğlu Takımı Motive Ediyor",
+aciklama:"Millî takım kaptanı Hakan Çalhanoğlu, takım içerisindeki birlik ve beraberliğin en büyük güçleri olduğunu ifade etti.",
+resim:"images/kerem.jpg",
+tarih:"2 Ağustos 2026"
 },
 
 {
-name:"Uğurcan Çakır",
-image:"images/ugurcan-cakir.jpg",
-position:"Kaleci",
-club:"Galatasaray",
-birth:"05.04.1996",
-caps:42,
-goals:0,
-number:23
-},
-
-/* =========================
-   DEFANS
-========================= */
-
-{
-name:"Abdülkerim Bardakcı",
-image:"images/abdulkerim-bardakci.jpg",
-position:"Defans",
-club:"Galatasaray",
-birth:"07.09.1994",
-caps:30,
-goals:2,
-number:14
+baslik:"İsmail Yüksek Hazırlıklarını Sürdürüyor",
+aciklama:"Başarılı orta saha oyuncusu İsmail Yüksek, kamp çalışmalarında gösterdiği performansla teknik heyetin beğenisini kazandı.",
+resim:"images/ismail-yuksek.jpg",
+tarih:"1 Ağustos 2026"
 },
 
 {
-name:"Çağlar Söyüncü",
-image:"images/caglar-soyuncu.jpg",
-position:"Defans",
-club:"Fenerbahçe",
-birth:"23.05.1996",
-caps:61,
-goals:2,
-number:4
+baslik:"Merih Demiral Savunmanın Lideri",
+aciklama:"Deneyimli stoper Merih Demiral, A Millî Takım savunmasının en önemli isimlerinden biri olmaya devam ediyor.",
+resim:"images/merih-demiral.jpg",
+tarih:"1 Ağustos 2026"
 },
 
 {
-name:"Eren Elmalı",
-image:"images/eren-elmali.jpg",
-position:"Defans",
-club:"Galatasaray",
-birth:"07.07.2000",
-caps:25,
-goals:0,
-number:13
+baslik:"Uğurcan Çakır Kaleyi Devralıyor",
+aciklama:"Başarılı file bekçisi Uğurcan Çakır, millî takım kampındaki performansıyla dikkat çekmeye devam ediyor.",
+resim:"images/ugurcan-cakir.jpg",
+tarih:"31 Temmuz 2026"
 },
 
 {
-name:"Ferdi Kadıoğlu",
-image:"images/ferdi-kadioglu.jpg",
-position:"Defans",
-club:"Brighton",
-birth:"07.10.1999",
-caps:32,
-goals:2,
-number:20
-},
-
-{
-name:"Merih Demiral",
-image:"images/merih-demiral.jpg",
-position:"Defans",
-club:"Al Ahli",
-birth:"05.03.1998",
-caps:65,
-goals:6,
-number:3
-},
-
-{
-name:"Mert Müldür",
-image:"images/mert-muldur.jpg",
-position:"Defans",
-club:"Fenerbahçe",
-birth:"03.04.1999",
-caps:48,
-goals:3,
-number:18
-},
-
-{
-name:"Ozan Kabak",
-image:"images/ozan-kabak.jpg",
-position:"Defans",
-club:"Hoffenheim",
-birth:"25.03.2000",
-caps:31,
-goals:2,
-number:15
-},
-
-{
-name:"Samet Akaydın",
-image:"images/samet-akaydin.jpg",
-position:"Defans",
-club:"Çaykur Rizespor",
-birth:"13.03.1994",
-caps:19,
-goals:1,
-number:25
-},
-
-{
-name:"Zeki Çelik",
-image:"images/zeki-celik.jpg",
-position:"Defans",
-club:"Roma",
-birth:"17.02.1997",
-caps:63,
-goals:3,
-number:2
-},
-/* =========================
-   ORTA SAHA
-========================= */
-
-{
-name:"Hakan Çalhanoğlu",
-image:"images/hakan-calhanoglu.jpg",
-position:"Orta Saha",
-club:"Inter",
-birth:"08.02.1994",
-caps:108,
-goals:22,
-number:10
-},
-
-{
-name:"İsmail Yüksek",
-image:"images/ismail-yuksek.jpg",
-position:"Orta Saha",
-club:"Fenerbahçe",
-birth:"26.01.1999",
-caps:34,
-goals:1,
-number:16
-},
-
-{
-name:"Kaan Ayhan",
-image:"images/kaan-ayhan.jpg",
-position:"Orta Saha",
-club:"Galatasaray",
-birth:"10.11.1994",
-caps:74,
-goals:6,
-number:22
-},
-
-{
-name:"Orkun Kökçü",
-image:"images/orkun-kokcu.jpg",
-position:"Orta Saha",
-club:"Beşiktaş",
-birth:"29.12.2000",
-caps:53,
-goals:5,
-number:6
-},
-
-{
-name:"Salih Özcan",
-image:"images/salih-ozcan.jpg",
-position:"Orta Saha",
-club:"Borussia Dortmund",
-birth:"11.01.1998",
-caps:32,
-goals:1,
-number:5
-},
-/* =========================
-   FORVET
-========================= */
-
-{
-name:"Kenan Yıldız",
-image:"images/kenan-yildiz.jpg",
-position:"Forvet",
-club:"Juventus",
-birth:"04.05.2005",
-caps:21,
-goals:3,
-number:10
-},
-
-{
-name:"Arda Güler",
-image:"images/arda-guler.jpg",
-position:"Forvet",
-club:"Real Madrid",
-birth:"25.02.2005",
-caps:20,
-goals:4,
-number:15
-},
-
-{
-name:"Barış Alper Yılmaz",
-image:"images/baris-alper-yilmaz.jpg",
-position:"Forvet",
-club:"Galatasaray",
-birth:"23.05.2000",
-caps:24,
-goals:3,
-number:53
-},
-
-{
-name:"Yusuf Sarı",
-image:"images/yusuf-sari.jpg",
-position:"Forvet",
-club:"Başakşehir",
-birth:"20.11.1998",
-caps:11,
-goals:1,
-number:7
-},
-
-{
-name:"Semih Kılıçsoy",
-image:"images/semih-kilicsoy.jpg",
-position:"Forvet",
-club:"Beşiktaş",
-birth:"15.08.2005",
-caps:10,
-goals:1,
-number:9
+baslik:"Orkun Kökçü'den Orta Sahaya Güç",
+aciklama:"Orkun Kökçü, pas kalitesi ve oyun görüşüyle Ay-Yıldızlı ekibin en önemli silahlarından biri olmayı sürdürüyor.",
+resim:"images/orkun-kokcu.jpg",
+tarih:"31 Temmuz 2026"
 }
 
 ];
