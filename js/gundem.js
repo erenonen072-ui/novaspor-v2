f70538ac8a3ee90592fed6a4c624127a44020627
erenonen072-ui <erenonen072@gmail.com>
2026-08-05 16:41:57 +0300

    Update gundem.js

--- js/gundem.js
+++ js/gundem.js
@@ -1,31 +1,9 @@
-// ======================================
-// NOVASPOR GÜNDEM
-// ======================================
-const gundemHaberleri = [
-    transferHaberleri[0],
-    superLigHaberleri[0],
-    lig2Haberleri[0],
-    lig1Haberleri[0]
+const transferHaberleri = [
+{
+    kategori: "TRANSFER",
+    baslik: "Trabzonspor'dan Tarihi Hamle! Mohamed Salah Transferinde Sona Doğru",
+    aciklama: "...",
+    resim: "images/muhhammed salah.png",
+    link: "https://sites.google.com/view/novassporr/haberler_1/trabzonspordan-tarihi-hamle-mohamed-salah-transferinde-sona-doğru?authuser=0"
+},
 ];
-
-const container = document.getElementById("gundem-container");
-
-if (container) {
-
-    container.innerHTML = "";
-
-    gundemHaberleri.forEach(haber => {
-
-        container.innerHTML += `
-            <a href="${haber.link}">
-                <img src="${haber.resim}" alt="${haber.baslik}">
-                <div>
-                    <span>${haber.kategori}</span>
-                    <h4>${haber.baslik}</h4>
-                </div>
-            </a>
-        `;
-
-    });
-
-}
